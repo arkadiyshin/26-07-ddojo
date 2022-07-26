896a5f5abc2dc9c43e10c07bdd43c1111b0e1848
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-07-26 17:22:57 +0200

    added animation

--- script.js
+++ script.js
@@ -59,6 +59,15 @@ class Button {
         this.button.style.width = "15em";
         this.button.style.height = "3em";
         this.button.style.order = this.order + 1;
+        this.button.style.position = "absolute";
+        //console.log(this.button.clientHeight);
+        //console.log(this.button.order);
+        this.button.style.top = 50 * parseInt(this.button.style.order-1) + 'px';
+        //console.log(this.button.style.top);
+        //this.button.style.left = ;
+        this.button.style.transition = "all 1s ease-in";
+
+
     }
 
     resetTimer() {
@@ -85,11 +94,26 @@ class Button {
 
     putButtonOnTheTop() {
         
-        const buttons = container.querySelectorAll("button");
+        /*const buttons = container.querySelectorAll("button");
         buttons.forEach( (e) => {
             e.style.order = parseInt(e.style.order)+1;
         });
-        this.button.style.order = 1;
+        this.button.style.order = 1;*/
+
+        const buttons = container.querySelectorAll("button");
+        buttons.forEach( (e) => {
+            
+            let top = e.style.top;
+            top = top.substring(0, top.length-2);
+            console.log(top);
+            //curTop = (e.style.top).substr(1, (e.style.top).length-2);
+            //numTop = parseInt(curTop) + 50;
+            e.style.top = parseInt(top)+ 50 + 'px';
+            console.log(e.style.top);
+        });
+        this.button.style.top = 0;
+
+
     }
 
 }
