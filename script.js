ec289cb355290fcc469d3f574f476037cf164fd6
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-07-27 09:05:18 +0200

    offset in separate function

--- script.js
+++ script.js
@@ -54,14 +54,16 @@ class Button {
 
     setStyle() {
 
-        this.button.style.margin = "0.5em";
+        this.button.style.margin = "10px";
         this.button.style.borderRadius = "0.5em";
-        this.button.style.width = "15em";
-        this.button.style.height = "3em";
+        this.button.style.width = "250px";
+        this.button.style.height = "50px";
         this.button.style.order = this.order + 1;
         this.button.style.position = "absolute";
-        this.button.style.top = 50 * parseInt(this.button.style.order-1) + 'px';
         this.button.style.transition = "all 1s ease-in";
+
+        this.button.style.top = this.getYOffset() * (parseInt(this.button.style.order)-1) + 'px';
+        
     }
 
     resetTimer() {
@@ -89,23 +91,22 @@ class Button {
     putButtonOnTheTop() {
         
         const buttons = container.querySelectorAll("button");
+        
         buttons.forEach( (e) => {
             if(parseInt(e.style.order) <= parseInt(this.button.style.order)) {
-            
-            let top = e.style.top;
-            top = top.substring(0, top.length-2);
-            e.style.top = parseInt(top)+ 50 + 'px';
-            console.log("top: " + e.style.top);
-            console.log("order: " + e.style.order);
+            e.style.top = parseInt(e.style.top) + this.getYOffset()+ 'px';
             }
         });
         this.button.style.top = 0;
 
-        //const buttons = container.querySelectorAll("button");
         buttons.forEach( (e) => {
             e.style.order = parseInt(e.style.order)+1;
         });
         this.button.style.order = 1;
+    }
+
+    getYOffset() {
+        return (parseInt(this.button.style.height) + 2 * parseInt(this.button.style.margin));    
     }
 
 }
